16256a4a971857b9b1a10cb0d4283b6d20b72697
Surazthakur <56506047+Surazthakur@users.noreply.github.com>
2020-09-03 22:32:01 +0530

    Update Assignment3.py
    Printing prime numbers between two intervals.

--- Assignment3.py
+++ Assignment3.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Assignment 3 : Print prime numbers between 1-200 using For loop and Range function 
+# Assignment 3 : Print prime numbers between 1-200 using For loop and Range function 
 
 # In[ ]:
-
-
 for i in range(0,200) :
     for j in range(2,i):
         if i / j == 0 :
