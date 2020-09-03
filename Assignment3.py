4c89232431f426669b05fd45d38e36fc8e2bec0f
Surazthakur <56506047+Surazthakur@users.noreply.github.com>
2020-09-03 22:07:10 +0530

    Add files via upload

---
+++ Assignment3.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Assignment 3 : Print prime numbers between 1-200 using For loop and Range function 
+
+# In[ ]:
+
+
+for i in range(0,200) :
+    for j in range(2,i):
+        if i / j == 0 :
+            break
+        else :
+            print(i)
+
