f7f77d3236e045edfdb3c6234b89a043c66e472e
Surazthakur <56506047+Surazthakur@users.noreply.github.com>
2020-09-08 21:06:04 +0530

    LetsUpgrade Python assignment 4

---
+++ Assignment 4.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Assignment 4 : Print armstrong number between the range of given numbers.
+
+# In[6]:
+
+
+lower = 1042000
+upper = 702648265
+for i in range(lower, upper):
+    num = i 
+    order = len(str(num))
+    temp = i 
+    sum = 0
+    while temp > 0  :
+        digit = temp % 10 
+        sum += digit ** order
+        temp //= 10
+    if num ==  sum :
+        print("The first armstrong number between the given range is :" , num)
+        break 
+
+
+# In[ ]:
+
+
+
+
