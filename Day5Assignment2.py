05cd9e78b8fca44d19c8f9c282708d0e3ee0da48
Surazthakur <56506047+Surazthakur@users.noreply.github.com>
2020-09-09 19:30:44 +0530

    LetsUpgrade Day 5 Assignment 2

---
+++ Day5Assignment2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Day 5 Assignment 2
+
+# In[1]:
+
+
+def isPrime(x):
+    for i in range(2,x):
+        if x % i == 0:
+            return False
+        else :
+            return True
+filterlst = filter(isPrime, range(2500))
+print('print number btw 1-2500 :', list(filterlst)) 
+
+
+# In[ ]:
+
+
+
+
