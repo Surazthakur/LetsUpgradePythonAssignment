0507a2456b79e2b5fd707b66284eb62bba0085d1
Surazthakur <56506047+Surazthakur@users.noreply.github.com>
2020-09-09 19:25:34 +0530

    LetsUpgrade Day 5 assignment 1

---
+++ Day5Assignment1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # DAY 5 Assignment 1
+
+# In[1]:
+
+
+newList = [1,5,6,4,1,2,4,3,5]
+subList = [1,1,5]
+if subList in newList :
+    print('It\'s  a match' )
+else :
+    print('It\'s not a match')    
+
+
+# In[ ]:
+
+
+
+
